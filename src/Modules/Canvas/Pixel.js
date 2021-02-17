665252a53e372362019d908149d95ea87532b587
Nathaniel Redmon <52013906+nonstopper0@users.noreply.github.com>
2021-02-17 08:13:43 -0700

    reworking pixel functionality

--- src/Modules/Canvas/Pixel.js
+++ src/Modules/Canvas/Pixel.js
@@ -3,12 +3,15 @@ import { findRenderedDOMComponentWithClass } from 'react-dom/test-utils'
 import './Pixel.scss'
 
 export default function Pixel(props) {
-    const { currentColor, dimension, zoom, pid, rid, brush } = props
-    const self = useRef();
+    const { currentColor, dimension, zoom, pid, rid } = props
     const [pixelColor, setPixelColor] = useState("#fff")
+    const self = useRef(null)
     const [oldColor, setOldColor] = useState(pixelColor)
     const [canChangeColor, setCanChangeColor] = useState(true);
     const [border, setBorder] = useState(false)
+
+    useEffect(() => {
+    }, [])
 
     function applyColor() {
         setPixelColor(currentColor)
@@ -16,12 +19,11 @@ export default function Pixel(props) {
         setBorder(false)
     }
 
-    async function changeColorOnHover(e) {
+    async function changeColorOnHover() {
+        console.log(self.current)
         setOldColor(pixelColor);
         setPixelColor(currentColor);
         setBorder(true)
-        let element = e.target.attributes[0]
-        console.log(e.target.getAttribute('value').split(","))
       }
     
       function resetColor() {
@@ -37,9 +39,9 @@ export default function Pixel(props) {
         ref={self}
         value={[rid, pid]}
         className="pixel"
-        onMouseEnter={changeColorOnHover}
-        onMouseLeave={resetColor}
-        onMouseDown={applyColor}
+        // onMouseEnter={changeColorOnHover}
+        // onMouseLeave={resetColor}
+        // onClick={applyColor}
         style={{boxShadow: border ? 'inset 0px 0px 0px 2px white' : '', backgroundColor: pixelColor, width: `${zoom/dimension}px`, height: `${zoom/dimension}px` }}
       ></div>
     )
--- src/Modules/Editor/Panels/Brush.js
+++ src/Modules/Editor/Panels/Brush.js
@@ -1,26 +1,52 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 
 export default function Brush() {
+    const [color, setColor] = useState('#bbbbbb')
+    const [mouseDown, click] = useState(false)
+    const [lastTime, setLastTime] = useState(0)
 
     // setup listener to listen for brush location
     useEffect(() => {
-        document.addEventListener('mousedown', handleMouse )
+        document.addEventListener('mousedown', () => click(true) )
+        document.addEventListener('mouseup', () => click(false) )
+        document.addEventListener('mousemove', handleDrag )
 
         return function cleanup() {
-            document.removeEventListener('mousedown', handleMouse )
+            document.removeEventListener('mousedown', handleClick )
+            document.removeEventListener('mousemove', handleDrag )
         }
     }, [])
 
-    function handleMouse(e) {
+    const handleDrag = (e) => {
+        if (Math.floor(e.timeStamp) % 10 === 0) {
+            console.log(mouseDown, lastTime)
+            if (mouseDown) {
+                console.log('drawing')
+                let below = document.elementFromPoint(e.clientX, e.clientY)
+                if (below.classList.contains("pixel")) {
+                    below.style.backgroundColor = color
+                }
+            }
+        }
+        if (lastTime == 0) {
+             setLastTime(e.timeStamp)
+        }
+
+    }
+    const handleClick = (e) => {
+        console.log('clicked')
+        click((previousState) => !previousState)
         let below = document.elementFromPoint(e.clientX, e.clientY)
         if (below.classList.contains("pixel")) {
-            below.style.backgroundColor = "black"
+            below.style.backgroundColor = color
         }
     }
 
 
     return(
-        <div>Brush</div>
+        <div>Brush
+            <button onClick={()=> console.log("hello")}></button>
+        </div>
     )
 }
