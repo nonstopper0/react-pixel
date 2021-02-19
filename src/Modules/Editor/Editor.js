30a29b5f257186f8df54a63f8f924c4e3c5a2631
Nathaniel Redmon <52013906+nonstopper0@users.noreply.github.com>
2021-02-18 23:08:58 -0700

    removed unnecessary state variables

--- src/Modules/Editor/Editor.js
+++ src/Modules/Editor/Editor.js
@@ -8,11 +8,10 @@ import Brush from './Panels/Brush';
 
 import { BsBrush } from 'react-icons/bs'
 
+// This is the main function all my panels and drawing components pull from. Think of this as the App() of this application.
 export default function Editor(props) {
     const [dimension, setDimension] = useState(props.dimension)
     const [zoom, setZoom] = useState(500)
-    const [top, setTop] = useState(50);
-    const [left, setLeft] = useState(50);
     const [currentColor, setCurrentColor] = useState('#bbbbbb')
     const [gridLines, setGridLines] = useState(false)
     const [hoverHelper, setHoverHelper] = useState(true);
@@ -68,7 +67,6 @@ export default function Editor(props) {
     // this code layout is super odd but it works perfectly? 
     // I think it is because the react state hooks dont update within the same function when using them this way so manipulating the setDropDown data works fine even when the checks later in the method dont work as the code maybe looks
     const handleOpenClose = (e, className, variable, setVar) => {
-        console.log(e.target, className, variable, setVar)
         if (variable) {
             setVar(false)
             let divToClose = document.querySelector(`.${variable}`)
